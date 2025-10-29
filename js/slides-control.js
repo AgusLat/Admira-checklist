c7b2fdf4b9e2ebf7368229e013526fe235742005
Agustin L <agus.lat89@gmail.com>
2025-10-29 11:37:09 +0100

    estructura de datos

---
+++ js/slides-control.js
@@ -0,0 +1,6 @@
+import { slidesSantaRosa } from "../slides/slides.js"
+
+export const nextSlide = ()=>{
+    console.log("test")
+    console.log(slidesSantaRosa.arcade[1].desc)
+}
---
+++ slides/slides.js
@@ -0,0 +1,40 @@
+
+  export const slidesSantaRosa =  {
+  sala360 : {
+    1: {desc: 'Encender el aire acondicionado de la sala 360 con el mando que hay en la estantería de Administración.', imgSrc: '/checklist_img/360_aire.jpg' },
+    2: {desc: 'Encender PC en forma de cubo de la sala de Administración. Esperar a que arranque e inicie el software de la sala 360.', imgSrc: '/checklist_img/360_pc_cubo.jpg' },
+    3: {desc: 'Encender los 5 proyectores con los mandos que hay bajo la mesa de la sala 360.', imgSrc: '/checklist_img/proyectores.jpg' },
+    4: {desc: 'Encender el portátil de la sala 360. Entrar con la contraseña: “admira123”. Dejar abierta la página del control del showroom colocando la URL: 192.168.1.82 en el navegador.', imgSrc: '/checklist_img/portatil.jpg' },
+    5: {desc: 'Cerrar la puerta de administración.', imgSrc: '/checklist_img/puerta-administracion.jpg' }
+  },
+  arcade :{
+    1: { desc: 'En la zona de almacén, colocar en el selector de llave la posición que mantiene las puertas cerradas hasta que se presiona el pulsador.', imgSrc: '/checklist_img/selector-llave-almacen.jpg' },
+    2: { desc: 'Poner la radio en modo CD, presionar play y pulsar “Rep” dos veces hasta que aparezca “REP-ALL”. Dejar el volumen en 14.', imgSrc: '/checklist_img/radio.jpg' },
+    3: { desc: 'Encender los dos aires acondicionados de la sala de arcade con el mando ubicado encima del Condensador De Flujo de Back To The Future (entrando a mano derecha).', imgSrc: '/checklist_img/aires.jpg' },
+    4: { desc: 'Encender Máquina Pong con el botón en la parte superior izquierda trasera. Conectar teclado, presionar F1 para entrar en BIOS, Esc para salir y Enter. El juego inicia automáticamente.', imgSrc: '/checklist_img/pong.jpg' },
+    5: { desc: 'En la TV con VHS, presionar stop y rebobinar la película de Back To The Future. Luego presionar play.', imgSrc: '/checklist_img/tv-bttf-vhs.jpg' },
+    6: { desc: 'Encender la TV Darth Vader y presionar “TV/DVD”. Seleccionar Español y Play Movie.', imgSrc: '/checklist_img/tv-darth-vader.jpg' },
+    7: { desc: 'Encender la Polybius usando la llave de Stranger Things. Abrir la carcasa, presionar el botón rojo de encendido y seleccionar Donkey Kong.', imgSrc: '/checklist_img/polybius.jpg' },
+    8: { desc: 'Encender cada máquina Viewlix (Arcades blancas) presionando el botón en la parte inferior izquierda del tablero de joysticks.', imgSrc: '/checklist_img/viewlix.jpg' },
+    9: { desc: 'Encender cada máquina Pinball pulsando los botones de encendido en la parte inferior de cada una.', imgSrc: '/checklist_img/pinballs.jpg' }
+  },
+  cafeteria:{
+    1: { desc: 'Encender el aire acondicionado con el mando situado detrás de la nevera.', imgSrc: '/checklist_img/aire.jpg' },
+    2: { desc: 'Enchufar el sofá rosa al entrar a mano izquierda, para encender las luces.', imgSrc: '/checklist_img/sofa-rosa.jpg' },
+    3: { desc: 'En la rockola, reproducir el álbum deseado. Presionar los números y los dos últimos dígitos deben ser “00”.', imgSrc: '/checklist_img/rockola.jpg' },
+    4: { desc: 'Comprobar que las 2 pantallas que simulan ventanas están encendidas.', imgSrc: '/checklist_img/ventanas.jpg' },
+    5: { desc: 'Asegurarse de que la puerta de acceso a los baños queda cerrada.', imgSrc: '/checklist_img/puerta-banos.jpg' }
+  },
+  garaje: {
+    1: { desc: 'Encender los aires acondicionados con los interruptores que se encuentran uno a cada lado de la pared del fondo.', imgSrc: '/checklist_img/aires.jpg' },
+    2: { desc: 'Encender cada máquina Pinball Stern, Tiburón, Jurassic Park, Tortugas Ninja y Mandalorian con el botón en la parte inferior.', imgSrc: '/checklist_img/pinballs.jpg' },
+    3: { desc: 'Encender la cabina Arcade Jurassic Park con el botón lateral.', imgSrc: '/checklist_img/arcade-jurassic.jpg' },
+    4: { desc: 'Encender la mesa de Pong con el botón de la parte lateral inferior.', imgSrc: '/checklist_img/mesa-pong.jpg' },
+    5: { desc: 'Comprobar que el PC de Tiburón está encendido y emitiendo en la pantalla grande.', imgSrc: '/checklist_img/pc-tiburon.jpg' },
+    6: { desc: 'Comprobar que está encendido el PC y la pantalla trasera del Jeep.', imgSrc: '/checklist_img/jeep-pantalla.jpg' },
+    7: { desc: 'Cerrar las puertas de la furgoneta de las TNT si hace falta.', imgSrc: '/checklist_img/furgo-tnt.jpg' },
+    8: { desc: 'Cerrar las puertas de los cuartillos detrás de la furgoneta de las TNT.', imgSrc: '/checklist_img/cuartillos.jpg' }
+  }
+};
+
+  
