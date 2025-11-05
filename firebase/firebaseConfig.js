d2fa7877503421856898b4a0f2c798b79a200b2f
Agustin L <agus.lat89@gmail.com>
2025-11-05 10:14:16 +0100

    feature(login):test login

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+      apiKey: "AIzaSyAqt1J5qs-wdCBjr1asU89URxVRzLPSVTY",
+      authDomain: "reservas-1b21a.firebaseapp.com",
+      projectId: "reservas-1b21a",
+      storageBucket: "reservas-1b21a.firebasestorage.app",
+      messagingSenderId: "751886584630",
+      appId: "1:751886584630:web:4f8cad44fc0ce4a24b0197",
+      measurementId: "G-JJ8LV8LYM6"
+    };
