41e5b8020d6abcc882a4d34145450ed2f2c9a0ca
Agustin L <agus.lat89@gmail.com>
2025-11-14 10:24:03 +0100

    templates-incidencias

---
+++ slides/slides-template.js
@@ -0,0 +1,116 @@
+export const templateSantaRosa = {
+  // SALA 360
+  "sala360_aire": 0,
+  "sala360_pc": 0,
+  "sala360_proyectores": 0,
+  "sala360_portatil": 0,
+  "sala360_puerta": 0,
+
+  // ARCADE
+  "arcade_selector": 0,
+  "arcade_radio": 0,
+  "arcade_aires": 0,
+  "arcade_pong": 0,
+  "arcade_vhs": 0,
+  "arcade_darthvader": 0,
+  "arcade_polybius": 0,
+  "arcade_viewlix": 0,
+  "arcade_pinballs": 0,
+
+  // CAFETERIA
+  "cafeteria_aire": 0,
+  "cafeteria_sofa": 0,
+  "cafeteria_rockola": 0,
+  "cafeteria_ventanas": 0,
+  "cafeteria_puerta_banos": 0,
+
+  // GARAJE
+  "garaje_aires": 0,
+  "garaje_pinballs": 0,
+  "garaje_mesa_pong": 0,
+  "garaje_pc_tiburon": 0,
+  "garaje_pc_jeep": 0,
+  "garaje_furgoneta": 0,
+  "garaje_cuartillos": 0
+};
+
+
+export const templateStore = {
+    
+  // METAHUMAN
+  "metahuman_aire": 0,
+  "metahuman_luces": 0,
+  "metahuman_ipad": 0,
+  "metahuman_metahuman": 0,
+
+  // RECEPCIÓN
+  "recepcion_pantalla": 0,
+  "recepcion_portatil": 0,
+};
+
+
+export const templatePlanetaTerminator = {
+  // ENTRADA
+  "entrada_puerta": 0,
+  "entrada_pias": 0,
+  "entrada_aire": 0,
+
+  // PANTALLAS / PC
+  "pantallas_moto": 0,
+  "pantallas_robots": 0,
+  "pantallas_humanos": 0,
+  "pantallas_pc": 0,
+  "pantallas_puertas": 0,
+};
+
+
+export const templatePlanetaNave = {
+  // ENTRADA
+  "entrada_pias": 0,
+  "entrada_telefonos": 0,
+  "entrada_musica": 0,
+  "entrada_pc": 0,
+  "entrada_holograma": 0,
+  "entrada_luz": 0,
+  "entrada_poster": 0,
+  "entrada_ascensor_abierto": 0,
+  "entrada_carcasa": 0,
+  "entrada_botones": 0,
+  "entrada_candado": 0,
+
+  // ASCENSOR
+  "ascensor_pantallas": 0,
+  "ascensor_mando": 0,
+  "ascensor_pc": 0,
+
+  // NAVE
+  "nave_puerta": 0,
+  "nave_display": 0,
+  "nave_luces": 0,
+  "nave_luces_color": 0,
+  "nave_videos": 0,
+  "nave_pc": 0,
+  "nave_tablet": 0,
+  "nave_tablet_encender": 0,
+  "nave_tablet_admira": 0,
+  "nave_astronauta": 0,
+  "nave_humo": 0,
+  "nave_cantina": 0,
+  "nave_r2d2": 0,
+  "nave_r2d2_cabeza": 0,
+  "nave_r2d2_mando": 0,
+  "nave_simulador": 0,
+
+  // OFICINA IT CROWD
+  "oficina_tv": 0,
+  "oficina_televisor": 0,
+  "oficina_alargue": 0,
+  "oficina_teclado": 0,
+  "oficina_puerta": 0,
+  "oficina_puertas": 0,
+  "oficina_cocina": 0,
+  "oficina_decorado": 0,
+  "oficina_pixelart": 0,
+  "oficina_timegate": 0,
+};
+
